62ad125f51e4a1ea8809b06264556cb129ddac48
NunesNathan <nunesxd7@hotmail.com>
2022-09-17 13:27:44 -0300

    fix: wrong imports

--- src/main/java/app/trybe/specialityapp/config/JerseyConfig.java
+++ src/main/java/app/trybe/specialityapp/config/JerseyConfig.java
@@ -1,10 +1,10 @@
 package app.trybe.specialityapp.config;
 
 import app.trybe.specialityapp.SpecialityAppApplication;
-import java.util.logging.Logger;
 import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.internal.inject.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.hibernate.boot.jaxb.internal.AbstractBinder;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
