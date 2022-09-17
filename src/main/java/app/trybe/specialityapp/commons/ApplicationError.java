15198e68fb91468726a67a3ed4a8926f60ebc04f
NunesNathan <nunesxd7@hotmail.com>
2022-09-17 14:00:53 -0300

    feat: application exception

--- src/main/java/app/trybe/specialityapp/commons/ApplicationError.java
+++ src/main/java/app/trybe/specialityapp/commons/ApplicationError.java
@@ -1,4 +1,25 @@
 package app.trybe.specialityapp.commons;
 
-public class ApplicationError {
+import javax.ws.rs.core.Response.Status;
+
+public class ApplicationError extends RuntimeException {
+  private Status status;
+  private String message;
+
+  /** app error constructor.*/
+  public ApplicationError(Status status, String message) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+
+  public ApplicationError() { }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
 }
