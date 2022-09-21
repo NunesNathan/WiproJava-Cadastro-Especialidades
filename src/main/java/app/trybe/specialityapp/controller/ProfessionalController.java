766a6586d0b0762a703254a0e33be345fca6d12c
NunesNathan <nunesxd7@hotmail.com>
2022-09-21 13:25:18 -0300

    fix: controller use autowired

--- src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
+++ src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
@@ -4,7 +4,6 @@ import app.trybe.specialityapp.commons.ApplicationError;
 import app.trybe.specialityapp.model.Professional;
 import app.trybe.specialityapp.service.ProfessionalService;
 import java.util.List;
-import java.util.NoSuchElementException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,7 +12,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,25 +19,21 @@ import org.springframework.stereotype.Controller;
 @Controller
 @Path("/professional")
 public class ProfessionalController {
-  private final ProfessionalService service;
 
   @Autowired
-  public ProfessionalController() {
-    this.service = new ProfessionalService();
-  }
+  private ProfessionalService professionalService;
 
   /** insert path.*/
   @POST
   @Path("/add")
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes("application/json")
+  @Produces("application/json")
   public Response insert(Professional p) {
     try {
-      String saved = service.save(p);
+      String saved = professionalService.save(p);
 
       return Response.status(Response.Status.CREATED).entity(saved).build();
     } catch (ApplicationError appError) {
-      System.out.println(appError.getStatus());
       return Response.status(appError.getStatus())
               .entity(appError)
               .build();
@@ -49,14 +43,13 @@ public class ProfessionalController {
   /** find all path.*/
   @GET
   @Path("/all")
-  @Produces(MediaType.APPLICATION_JSON)
+  @Produces("application/json")
   public Response findAll() {
     try {
-      List<Professional> professionals = service.list();
+      List<Professional> professionals = professionalService.list();
 
       return Response.status(Response.Status.OK).entity(professionals).build();
     } catch (ApplicationError appError) {
-      System.out.println(appError.getStatus().getStatusCode());
       return Response.status(appError.getStatus())
               .entity(appError)
               .build();
@@ -66,15 +59,14 @@ public class ProfessionalController {
   /** update book method.*/
   @PUT
   @Path("/edit/{id}")
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes("application/json")
+  @Produces("application/json")
   public Response edit(@PathParam("id") Integer id, Professional toUpdate) {
     try {
-      String updated = service.update(toUpdate, id);
+      String updated = professionalService.update(toUpdate, id);
 
       return Response.status(Response.Status.OK).entity(updated).build();
     } catch (ApplicationError appError) {
-      System.out.println(appError.getStatus());
       return Response.status(appError.getStatus())
               .entity(appError)
               .build();
@@ -84,15 +76,14 @@ public class ProfessionalController {
   /** delete book method.*/
   @DELETE
   @Path("/delete/{id}")
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes("application/json")
+  @Produces("application/json")
   public Response delete(@PathParam("id") Integer id) {
     try {
-      String updated = service.delete(id);
+      String deleted = professionalService.delete(id);
 
-      return Response.status(Response.Status.OK).entity(updated).build();
+      return Response.status(Response.Status.OK).entity(deleted).build();
     } catch (ApplicationError appError) {
-      System.out.println(appError.getStatus());
       return Response.status(appError.getStatus())
               .entity(appError)
               .build();
